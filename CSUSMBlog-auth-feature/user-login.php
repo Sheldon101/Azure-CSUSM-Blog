82066fa3a24d3647ac316afacef898e5461ca877
Sheldon101 <53189556+Sheldon101@users.noreply.github.com>
2023-05-08 19:17:45 -0700

    Update user-login.php

--- CSUSMBlog-auth-feature/user-login.php
+++ CSUSMBlog-auth-feature/user-login.php
@@ -23,7 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die($message);
     }
 
- //this code for sql server copy    
     // Connect to SQL server
     try {
           $conn = new PDO("sqlsrv:server = tcp:csusm-server.database.windows.net,1433; Database = BLOG_CSUSM", "Citla", "{PASSword1#}");
@@ -36,7 +35,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $connectionInfo = array("UID" => "Citla", "pwd" => "{PASSword1#}", "Database" => "BLOG_CSUSM", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
     $serverName = "tcp:csusm-server.database.windows.net,1433";
     $conn = sqlsrv_connect($serverName, $connectionInfo);
-    ///up to here 
+    
 
     $statement= $pdo->prepare("SELECT * FROM Admin WHERE username=:username AND userPassword=:userPassword");
     $statement->execute([
